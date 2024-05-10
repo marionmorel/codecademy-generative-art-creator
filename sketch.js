afae5b3ea7de07a6953340b64e8ababc62235909
ma-mrl <marion.mrl@live.fr>
2024-05-10 18:13:58 +0200

    getting started

--- sketch.js
+++ sketch.js
@@ -6,9 +6,14 @@ let angle = 0;
 
 function setup() {
   // TODO: Create canvas and set initial background color
+    createCanvas(windowWidth, windowHeight);
+    background(50, 50, 50);
 
   // TODO: Randomize variables used to draw shapes
-
+    x = Math.floor(windowWidth);
+    y = Math.floor(windowHeight);
+    speedX = random(-3, 3);
+    speedY = random(-3, 3);
 }
 
 function draw() {
